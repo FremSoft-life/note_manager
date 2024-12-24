8b1f2d1a51b462a29523de311b94588486731d0f
FremSoft-life <ffadee578@gmail.com>
2024-12-24 23:37:39 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,17 @@
+name = 'Arni'
+print (name)
+
+title = 'DZ'
+print (title)
+
+content = 'переменые'
+print (content)
+
+status = 'в процессе'
+print (status)
+
+created_date = 'дата создания заметки'
+print (created_date)
+
+issue_date = 'дата истечения заметки (дедлайн)'
+print (issue_date)
