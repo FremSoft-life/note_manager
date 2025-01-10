b066cbb967877d8cab06fcfe76f43934f00cd99e
FremSoft-life <ffadee578@gmail.com>
2025-01-10 21:47:57 +0300

    Create add_input.py
    Задание 3

---
+++ add_input.py
@@ -0,0 +1,14 @@
+from importlib.resources import contents
+
+username=input()
+#"ffadeev1998@mail.ru"
+title=input()
+#"заголовок заметки"
+contents=input()
+#"Текст заметки"
+status=input()
+#"Готово"
+created_date=input()
+#("29.12.2024")
+issue_date=input()
+#("29.12.2024")
