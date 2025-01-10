6fc6cdd47987ba3ecca802dad158cdaedcfdd3f4
FremSoft-life <ffadee578@gmail.com>
2025-01-10 21:33:27 +0300

    Create final.py
    Задание 5

---
+++ final.py
@@ -0,0 +1,14 @@
+new_list_of_employees = []
+employee_data = ["ФИО", "Дата рождения", "Место рождения", "Место проживания", "Номер телефона", "Образование"]
+print (employee_data)
+print('\n'.join(employee_data))
+list_numbering = (1, 2, 3, 4, 5, 6, 7)
+
+print (list_numbering)
+
+print (list_numbering[0], employee_data[0])
+print (list_numbering[1], employee_data[1])
+print (list_numbering[2], employee_data[2])
+print (list_numbering[3], employee_data[3])
+print (list_numbering[4], employee_data[4])
+print (list_numbering[5], employee_data[5])
