7e547b140a1613a48bbabe9263a76aaef0f7b202
FremSoft-life <ffadee578@gmail.com>
2025-01-21 22:58:13 +0300

    Update add_input.py
    Этап 1. Задание 3

--- add_input.py
+++ add_input.py
@@ -1,14 +1,39 @@
-from importlib.resources import contents
+# add_input.py
 
-username=input()
-#"ffadeev1998@mail.ru"
-title=input()
-#"заголовок заметки"
-contents=input()
-#"Текст заметки"
-status=input()
-#"Готово"
-created_date=input()
-#("29.12.2024")
-issue_date=input()
-#("29.12.2024")
+def get_note_data():
+    """Запрашивает у пользователя данные для создания заметки."""
+
+    title = input("Введите заголовок заметки:Информация о объекте")
+    content = input("Введите текст заметки:ТТХ объекта ")
+    tags = input("Высота, ширина, длина").split(",")
+    tags = [tag.strip() for tag in tags] # Удаляем пробелы вокруг тегов
+
+    while True:
+        date_str = input("21.01.2025")
+        try:
+            date = date_str
+            break
+        except ValueError:
+            print("Неверный формат даты. Пожалуйста, используйте формат ГГГГ-ММ-ДД.")
+
+    status = input("Введите статус заметки: выполнено")
+
+
+    return {
+        "title": title,
+        "content": content,
+        "tags": tags,
+        "date": date,
+        "status": status,
+    }
+
+
+
+def main():
+    note_data = get_note_data()
+    print("\nСоздана заметка:")
+    for key, value in note_data.items():
+        print(f"{key}: {value}")
+
+if __name__ == "__main__":
+    main()
