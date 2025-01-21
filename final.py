337d3fb72125def9772ca643f0f3f15c547acfd9
FremSoft-life <ffadee578@gmail.com>
2025-01-21 23:27:00 +0300

    Update final.py
    Этап 1. Задание 5

--- final.py
+++ final.py
@@ -1,14 +1,59 @@
-new_list_of_employees = []
-employee_data = ["ФИО", "Дата рождения", "Место рождения", "Место проживания", "Номер телефона", "Образование"]
-print (employee_data)
-print('\n'.join(employee_data))
-list_numbering = (1, 2, 3, 4, 5, 6, 7)
+# final.py
 
-print (list_numbering)
+def get_note_data():
+    """Запрашивает у пользователя данные для создания заметки и возвращает их в виде списка."""
 
-print (list_numbering[0], employee_data[0])
-print (list_numbering[1], employee_data[1])
-print (list_numbering[2], employee_data[2])
-print (list_numbering[3], employee_data[3])
-print (list_numbering[4], employee_data[4])
-print (list_numbering[5], employee_data[5])
+    note = []
+
+    name = input("Руководитель эксперимента Професор Койманов")
+    note.append(name)
+
+    content = input("Анализ работы на объектом 1-442")
+    note.append(content)
+
+    status = input("Анализ успешен, переходим ко второму этапу")
+    note.append(status)
+
+    created_date = input("Дата начала проведения анализа 10.01.2025")
+    note.append(created_date)
+
+
+    modified_date = input("Дата окончания проведения анализа 12.01.2025")
+    note.append(modified_date)
+
+    name = input("Руководитель эксперимента Професор Койманов")
+    note.append(name)
+
+    content = input("Анализ работы на объектом 1-543")
+    note.append(content)
+
+    status = input("Анализ отрицательный, необходима утилизация")
+    note.append(status)
+
+    created_date = input("Дата начала проведения анализа 14.01.2025")
+    note.append(created_date)
+
+    modified_date = input("Дата окончания проведения анализа 15.01.2025")
+    note.append(modified_date)
+
+    titles = []
+    num_titles = int(input("4"))
+    for i in range(num_titles):
+        title = input(f"Введите заголовок {i + 1}: ")
+        titles.append(title)
+    note.append(titles)
+
+    return note
+
+def display_note(note):
+    """Выводит информацию о заметке на экран."""
+    labels = ["Имя пользователя", "Содержание", "Статус", "Дата создания", "Дата изменения", "Заголовки"]
+    for i in range(len(labels)):
+        print(f"{labels[i]}: {note[i]}")
+
+def main():
+    note = get_note_data()
+    display_note(note)
+
+if __name__ == "__main__":
+    main()
