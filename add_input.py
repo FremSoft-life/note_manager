b97464e3ac0464860dc6dfbd871debb8e0a9b835
FremSoft-life <ffadee578@gmail.com>
2025-01-21 23:04:25 +0300

    Update add_input.py
    Этап 1. Задание 3

--- add_input.py
+++ add_input.py
@@ -9,22 +9,22 @@ def get_note_data():
     tags = [tag.strip() for tag in tags] # Удаляем пробелы вокруг тегов
 
     while True:
-        date_str = input("21.01.2025")
-        try:
-            date = date_str
-            break
-        except ValueError:
-            print("Неверный формат даты. Пожалуйста, используйте формат ГГГГ-ММ-ДД.")
+    date_str = input("21.01.2025")
+    try:
+    date = date_str
+    break
+    except ValueError:
+    print("Неверный формат даты. Пожалуйста, используйте формат ГГГГ-ММ-ДД.")
 
     status = input("Введите статус заметки: выполнено")
 
 
     return {
-        "title": title,
-        "content": content,
-        "tags": tags,
-        "date": date,
-        "status": status,
+    "title": title,
+    "content": content,
+    "tags": tags,
+    "date": date,
+    "status": status,
     }
 
 
@@ -33,7 +33,7 @@ def main():
     note_data = get_note_data()
     print("\nСоздана заметка:")
     for key, value in note_data.items():
-        print(f"{key}: {value}")
+    print(f"{key}: {value}")
 
 if __name__ == "__main__":
     main()
