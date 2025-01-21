e290fcd4d531e6c939e8c20294d4a999357a5a92
FremSoft-life <ffadee578@gmail.com>
2025-01-21 23:06:15 +0300

    Update add_list.py
    Этап 3. Задание 4

--- add_list.py
+++ add_list.py
@@ -1,10 +1,40 @@
-info = [1, 2, 3]
-info2 = [1, 1.7, 'Строка', [4, 5, 'Строка 2']]
+# add_list.py
 
-names = ['Название произведения', 'Введение',
-         'Оглавление',
-         ]
-print(names[:])
+def get_note_data():
+    """Запрашивает у пользователя данные для создания заметки, включая несколько заголовков."""
 
-result = names.append('Глова 1,Глова 2,Глова 3')
-print(names)
+    titles = []
+    for i in range(3): # Запрашиваем 3 заголовка
+    title = input(f"Введите заголовок {i+1}: ")
+    titles.append(title)
+
+    content = input("Введите текст заметки: Информация об объекте ")
+    tags = input("Высота, длина, ширина").split(",")
+    tags = [tag.strip() for tag in tags]
+
+    while True:
+    date_str = input("21.01.2025")
+    try:
+    date = date_str
+    break
+    except ValueError:
+    print("Неверный формат даты. Пожалуйста, используйте формат ГГГГ-ММ-ДД.")
+
+    status = input("Выполнено")
+
+    return {
+    "titles": titles, # Список заголовков
+    "content": content,
+    "tags": tags,
+    "date": date,
+    "status": status,
+    }
+
+    def main():
+    note_data = get_note_data()
+    print("\nСоздана заметка:")
+    for key, value in note_data.items():
+    print(f"{key}: {value}")
+
+if __name__ == "__main__":
+    main()
